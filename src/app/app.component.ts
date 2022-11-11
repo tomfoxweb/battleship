644d36af1cac7330ec13fa029cb2a888be75c6e0
Tom <tomwansww@gmail.com>
2022-11-11 15:56:28 +0500

    Add game images and show them on ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'battleship';
+  playerMap = new Array<number>(81);
+  opponentMap = new Array<number>(81);
 }
